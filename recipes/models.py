5e06f032939e8a3deecce415e2ed912d98f316b6
Luiz Fernando Dezorze <luizferzorze@gmail.com>
2022-07-08 07:19:08 -0300

    setting models and django admin

--- recipes/models.py
+++ recipes/models.py
@@ -1,14 +1,19 @@
-from pickle import TRUE
-from statistics import mode
-
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+    def __str__(self):
+        return self.name
 
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
     slug = models.SlugField()
-    preparation_time = models.models.IntegerField()
+    preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
@@ -18,3 +23,12 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True
+    )
+    author = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True
+    )
+
+    def __str__(self):
+        return self.title
