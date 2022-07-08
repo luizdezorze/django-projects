2b8266c0e2bef38ce50536b7c4b62b9cee882ba2
Luiz Fernando Dezorze <luizferzorze@gmail.com>
2022-07-08 20:27:19 -0300

    new changes

--- recipes/views.py
+++ recipes/views.py
@@ -1,5 +1,4 @@
-from typing import Tuple
-
+from django.http import Http404
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
@@ -16,8 +15,13 @@ def home(request):
 def category(request, category_id):
     recipes = Recipe.objects.filter(
         category__id=category_id, is_published=True).order_by('-id')
+
+    if not recipes:
+        raise Http404('Not Found😢')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name} - Category |'
     })
 
 
